11434b67d4d222278f93f8e32fc8fa50be76252b
houssemradhouane <radhouane.houssem@gmail.com>
2021-05-10 04:17:22 +0200

    Base de données connectée

--- BaseUtilisateurs.java
+++ BaseUtilisateurs.java
@@ -24,45 +24,90 @@ public class BaseUtilisateurs {
     }
     
     /**
-	 * Vérifie si l'identifiant donné est utilisé par un étudiant
+	 * Vérifie si l'identifiant donné est utilisé par un étudiant.
 	 * 
 	 * @param usern l'identifiant
-	 * @return
+	 * @return true si l'identifiant est utilisé par un étudiant
 	 */
     public boolean etudiantExistant(String usern) {
     	try {
-    	  ResultSet resultset = Base.select("SELECT identifiant FROM etudiant WHERE identifiant = " + usern);
-    	return resultset != null;
+    	  ResultSet resultset = Base.select("SELECT identifiant FROM users.etudiant WHERE identifiant = '" + usern+"'");
+    	  return (resultset.next());
     	} catch(SQLException e) {
-    		System.out.println("Error in querry");
+    		System.out.println(e.getMessage());
     	    return false;	
     	}
     }
     
     /**
-	 * Vérifie si l'identifiant donné est utilisé par un enseignant
+	 * Vérifie si l'identifiant donné est utilisé par un enseignant.
 	 * 
 	 * @param usern l'identifiant
-	 * @return
+	 * @return true si l'identifiant est utilisé par un enseignant
 	 */
     public boolean enseignantExistant(String usern) {
     	try {
-    	  ResultSet resultset = Base.select("SELECT enseignant FROM etudiant WHERE identifiant = " + usern);
-    	return resultset != null;
+    	  ResultSet resultset = Base.select("SELECT identifiant FROM users.enseignant WHERE identifiant = '" + usern+"'");
+    	  return (resultset.next());
     	} catch(SQLException e) {
-    		System.out.println("Error in querry");
+    		System.out.println(e.getMessage());
     	    return false;	
     	}
     }
     
     /**
-	 * Vérifie si l'identifiant donné est utilisé par un utilisateur
+	 * Vérifie si l'identifiant donné est utilisé par un utilisateur.
 	 * 
 	 * @param usern l'identifiant
-	 * @return
+	 * @return true si l'identifiant est utilisé
 	 */
     public boolean utilisateurExistant(String usern) {
-    	return enseignantExistant(usern) || etudiantExistant(usern);
+    	return (this.enseignantExistant(usern) || this.etudiantExistant(usern));
     }
 
+    /**
+	 * ajoute un nouveau étudiant au tableau étudiant
+	 * 
+	 * @param usern l'identifiant
+	 * @param mdp le mot de passe
+	 * @param nom le nom
+	 * @param prenom le prenom
+	 * @param ens l'identifiant de l'enseignant responsable
+	 */
+    public void ajouterEtudiant(String usern, String mdp, String nom, String prenom, String ens) {
+    	if (!utilisateurExistant(usern)) {
+    		try {
+    			int status = Base.createOrUpdateOrDelete("INSERT INTO users.etudiant (identifiant, nom, prenom, mdp, enseignant) "
+    					+ "VALUES ('"+usern+"', '"+nom+"', '"+prenom+"', '"+mdp+"', '"+ens+"')");
+    		} catch(SQLException e) {
+        		System.out.println(e.getMessage());
+    		}
+    	}
+    }
+    
+    /**
+	 * ajoute un nouveau enseignant au tableau étudiant
+	 * 
+	 * @param usern l'identifiant
+	 * @param mdp le mot de passe
+	 * @param nom le nom
+	 * @param prenom le prenom
+	 */
+    public void ajouterEnseignant(String usern, String mdp, String nom, String prenom) {
+    	if (!utilisateurExistant(usern)) {
+    		try {
+    			int status = Base.createOrUpdateOrDelete("INSERT INTO users.enseignant (identifiant, nom, prenom, mdp) "
+    					+ "VALUES ('"+usern+"', '"+nom+"', '"+prenom+"', '"+mdp+"')");
+    		} catch(SQLException e) {
+        		System.out.println(e.getMessage());
+    		}
+    	}
+    }
+    
+    /**
+	 * Se déconnecter de la base de données utilisateurs
+	 */
+	public void disconnect() {
+		this.Base.disconnect();
+	}
 }
---
+++ LoginGUI.java
@@ -0,0 +1,41 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class LoginGUI {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LoginGUI window = new LoginGUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public LoginGUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 938, 656);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
